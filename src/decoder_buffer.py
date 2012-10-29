2e35f46029843de997f5617d27a7998265d56b3e
S Pradeep Kumar <gohanpra@gmail.com>
2012-10-29 16:52:33 +0530

    Added a DecoderBuffer class.

---
+++ src/decoder_buffer.py
@@ -0,0 +1,22 @@
+from stage_buffer import StageBuffer
+from pprint import pprint
+
+class DecoderBuffer(StageBuffer):
+    """Buffer to store output of Decode stage.
+    """
+    arg_list = [
+        'instr',
+        'is_decoder_stalled',
+        'register_file',
+        'fetcher_buffer',
+        'npc',
+        'register_file',
+        'rs',
+        'rt',
+        ]
+    
+    def __init__(self, input_dict = {}):
+        """
+        """
+        super(FetcherBuffer, self).__init__(input_dict)
+
